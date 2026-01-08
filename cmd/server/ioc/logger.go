6bf8cf8ce1112f91a087201eb198e085ff207ebd
sinsoledad <3033056936@qq.com>
2026-01-08 22:10:07 +0800

    refactor(logger): 重构日志组件集成 OpenTelemetry 支持
    - 将 logger_zap.go 和 logger_nop.go 重命名为 zap.go 和 nop.go
    - 在 InitLogger 中集成 otelzap 以支持 OpenTelemetry 追踪
    - 配置 Zap 编码器使用彩色级别显示
    - 添加 WithTraceIDField 配置以自动加入 trace_id 字段到日志
    - 替换全局 Logger 以便第三方库使用
    - 更新代码结构以返回 OtelZapLogger 适配器

--- cmd/server/ioc/logger.go
+++ cmd/server/ioc/logger.go
@@ -2,34 +2,63 @@ package ioc
 
 import (
 	"bedrock/pkg/logger"
+
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
+// InitLogger 初始化 Logger 组件
+// 注意：返回值是接口 logger.Logger，实现了依赖倒置
 func InitLogger() logger.Logger {
-	//type LogConfig struct {
-	//	Level      string `mapstructure:"level"`
-	//	Filename   string `mapstructure:"filename"`
-	//	MaxSize    int    `mapstructure:"max_size"`
-	//	MaxAge     int    `mapstructure:"max_age"`
-	//	MaxBackups int    `mapstructure:"max_backups"`
-	//}
-	//var customCfg LogConfig
-	//viper.UnmarshalKey("log", &customCfg)
-
-	//// 这里我们用一个小技巧，
-	//// 就是直接使用 zap 本身的配置结构体来处理
-	//cfg := zap.NewDevelopmentConfig()
-	////err := viper.UnmarshalKey("log", &cfg)
-	////if err != nil {
-	////	panic(err)
-	////}
-	//l, err := cfg.Build()
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	if err := InitZap("debug"); err != nil {
+	// 1. 配置原生 Zap
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	baseLogger, err := cfg.Build()
+	if err != nil {
 		panic(err)
 	}
 
-	return logger.NewZapLogger(lg)
+	// 2. 包装为 otelzap
+	// WithTraceIDField(true) 确保 trace_id 字段被自动加入日志
+	otelLogger := otelzap.New(
+		baseLogger,
+		otelzap.WithMinLevel(zap.DebugLevel),
+	)
+
+	// (可选) 替换全局 Logger，方便第三方库使用
+	otelzap.ReplaceGlobals(otelLogger)
+
+	// 3. 返回我们的适配器
+	return logger.NewOtelZapLogger(otelLogger)
 }
+
+// func InitLogger() logger.Logger {
+// 	//type LogConfig struct {
+// 	//	Level      string `mapstructure:"level"`
+// 	//	Filename   string `mapstructure:"filename"`
+// 	//	MaxSize    int    `mapstructure:"max_size"`
+// 	//	MaxAge     int    `mapstructure:"max_age"`
+// 	//	MaxBackups int    `mapstructure:"max_backups"`
+// 	//}
+// 	//var customCfg LogConfig
+// 	//viper.UnmarshalKey("log", &customCfg)
+
+// 	//// 这里我们用一个小技巧，
+// 	//// 就是直接使用 zap 本身的配置结构体来处理
+// 	//cfg := zap.NewDevelopmentConfig()
+// 	////err := viper.UnmarshalKey("log", &cfg)
+// 	////if err != nil {
+// 	////	panic(err)
+// 	////}
+// 	//l, err := cfg.Build()
+// 	//if err != nil {
+// 	//	panic(err)
+// 	//}
+
+// 	if err := InitZap("debug"); err != nil {
+// 		panic(err)
+// 	}
+
+// 	return logger.NewZapLogger(lg)
+// }
